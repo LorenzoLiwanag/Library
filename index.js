627cc592045263d3c4384d883ace88ec9e5eefab
LorenzoLiwanag <lorenzonlliwanag@outlook.com>
2025-04-23 19:55:42 -0600

    folder structure and basic uI

---
+++ index.js
@@ -0,0 +1,5 @@
+const addBookBtn = document.getElementById("addBookBtn");
+const addBookDialog = document.getElementById("addBookDialog");
+addBookBtn.addEventListener("click", () => {
+    addBookDialog.showModal();
+})
