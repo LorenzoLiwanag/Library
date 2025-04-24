6c8a8d5dca7e15955837187aafab1304265ca639
LorenzoLiwanag <lorenzonlliwanag@outlook.com>
2025-04-23 20:10:13 -0600

    declared ids, variables and book object

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-const addBookBtn = document.getElementById("addBookBtn");
+const showDialogBtn = document.getElementById("showDialogBtn");
 const addBookDialog = document.getElementById("addBookDialog");
-addBookBtn.addEventListener("click", () => {
+const bookTitle = document.getElementById("bookTitle");
+const bookAuthor = document.getElementById("bookAuthor");
+const bookPageCount = document.getElementById("bookPageCount");
+const hasBeenRead = document.getElementsById ("hasBeenRead");
+const hasNotBeenRead = document.getElementById("hasNotBeenRead");
+const addBookBtn = document.getElementById("addBook");
+
+const myLibrary = [];
+
+function Book (title, author, numOfPages, read) {
+    this.title = title;
+    this.author = author;
+    this.numOfPages = numOfPages;
+    this.read = read;
+}
+
+showDialogBtn.addEventListener("click", () => {
     addBookDialog.showModal();
 })
+
+
+
