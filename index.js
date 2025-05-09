5f05d52beb4634ca2a436db648e28d8d272094f4
LorenzoLiwanag <lorenzonlliwanag@outlook.com>
2025-05-08 21:14:05 -0600

    added read/not read

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const addBookDialog = document.getElementById("addBookDialog");
 const bookTitle = document.getElementById("bookTitle");
 const bookAuthor = document.getElementById("bookAuthor");
 const bookPageCount = document.getElementById("bookPageCount");
-// const hasBeenRead = document.getElementById ("hasBeenRead");
-// const hasNotBeenRead = document.getElementById("hasNotBeenRead");
+const hasBeenRead = document.getElementById("hasBeenRead");
+const hasNotBeenRead = document.getElementById("hasNotBeenRead");
 const addBookBtn = document.getElementById("addBook");
 
 const library = document.querySelector("#library");
@@ -12,11 +12,11 @@ const library = document.querySelector("#library");
 
 const myLibrary = [];
 
-function Book(title, author, numOfPages) {
+function Book(title, author, numOfPages, read) {
     this.title = title;
     this.author = author;
     this.numOfPages = numOfPages;
-    // this.read = read;
+    this.read = read;
 }
 
 showDialogBtn.addEventListener("click", () => {
